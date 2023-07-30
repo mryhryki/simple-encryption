ec550f7e9f3966ed0d9877f40d63e4b5ee7ee8ea
mryhryki <mryhryki@gmail.com>
2023-07-30 09:37:17 +0900

    Fix assertion error message

--- src/test/common.ts
+++ src/test/common.ts
@@ -21,6 +21,6 @@ export interface Test {
 
 export const assert = (v1: unknown, v2: unknown): void => {
   if (JSON.stringify(v1) !== JSON.stringify(v2)) {
-    throw new Error(`Assertion failed: ${v1} !== ${v2}`);
+    throw new Error(`Assertion failed: ${JSON.stringify(v1)} !== ${JSON.stringify(v2)}`);
   }
 };
