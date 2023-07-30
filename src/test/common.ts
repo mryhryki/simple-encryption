4cb668858db78e9d45a70212b8cf0ea172691213
mryhryki <mryhryki@gmail.com>
2023-07-30 10:01:48 +0900

    Fix code format

--- src/test/common.ts
+++ src/test/common.ts
@@ -21,6 +21,8 @@ export interface Test {
 
 export const assert = (v1: unknown, v2: unknown): void => {
   if (JSON.stringify(v1) !== JSON.stringify(v2)) {
-    throw new Error(`Assertion failed: ${JSON.stringify(v1)} !== ${JSON.stringify(v2)}`);
+    throw new Error(
+      `Assertion failed: ${JSON.stringify(v1)} !== ${JSON.stringify(v2)}`,
+    );
   }
 };
