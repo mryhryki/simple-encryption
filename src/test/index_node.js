4da97a61783dad272408dc0c2220fd0b924544a8
mryhryki <mryhryki@gmail.com>
2023-08-05 23:44:05 +0900

    Update src/test/index_node.js

--- src/test/index_node.js
+++ src/test/index_node.js
@@ -1,6 +1,14 @@
-import { webcrypto } from "node:crypto";
+import * as crypto from "crypto";
 import { test } from "./index.js";
 import { decrypt, encrypt } from "@mryhryki/simple-encryption";
 
-globalThis.crypto = webcrypto;
+// Node.js v19 and later, there is `crypto` in `globalThis`.
+//
+// Ref:
+//   - https://github.com/nodejs/node/pull/42083
+//   - https://github.com/nodejs/node/blob/main/doc/changelogs/CHANGELOG_V19.md#19.0.0
+if (!("crypto" in globalThis)) {
+  globalThis.crypto = crypto.webcrypto;
+}
+
 test({ encrypt, decrypt });
