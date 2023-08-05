4a2d624733fd74d977c63ec7496e18ac952b1d4f
mryhryki <mryhryki@gmail.com>
2023-08-06 00:52:38 +0900

    Update script/bundle_test.ts

--- script/bundle_test.ts
+++ script/bundle_test.ts
@@ -1,5 +1,4 @@
 import { bundle } from "https://deno.land/x/emit/mod.ts";
 
 const { code } = await bundle("./src/test/index.ts");
-await Deno.mkdir("./dist/", { recursive: true });
 await Deno.writeTextFile("./src/test/index.js", code);
