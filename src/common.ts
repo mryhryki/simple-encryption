28f68f4b27152a834cccd8a68281e5aac8795766
mryhryki <mryhryki@gmail.com>
2025-10-13 08:16:53 +0900

    Update src/common.ts

--- src/common.ts
+++ src/common.ts
@@ -3,7 +3,7 @@ import type { SimpleEncryptionType } from "./types.d.ts";
 export const DefaultAlg: Readonly<SimpleEncryptionType.SupportAlgorithm> =
   "AES-GCM" as const;
 
-export const getRandomBytes = (length: number, crypto: Crypto): Uint8Array => {
+export const getRandomBytes = (length: number): Uint8Array => {
   const arr = new Uint8Array(length);
   crypto.getRandomValues(arr);
   return arr;
@@ -13,7 +13,6 @@ export const getRandomBytes = (length: number, crypto: Crypto): Uint8Array => {
 export const getKey = (
   key: Uint8Array,
   alg: string,
-  crypto: Crypto,
 ): Promise<CryptoKey> =>
   crypto.subtle.importKey("raw", key.buffer as ArrayBuffer, alg, true, [
     "encrypt",
